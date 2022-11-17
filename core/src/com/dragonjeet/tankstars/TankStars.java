1a36f60581ea4cb8083bcf574f8ceb87790f107a
viksit-siddhant <s.r.viksit@gmail.com>
2022-11-17 17:53:07 +0530

    Finished Scene2D UI

--- core/src/com/dragonjeet/tankstars/TankStars.java
+++ core/src/com/dragonjeet/tankstars/TankStars.java
@@ -16,7 +16,7 @@ public class TankStars extends ApplicationAdapter {
 	private Stage stage;
 	private Button menuButton;
 	private Skin skin;
-	private Table table;
+	private Table root;
 
 	@Override
 	public void create () {
@@ -24,23 +24,45 @@ public class TankStars extends ApplicationAdapter {
 		skin = new Skin(Gdx.files.internal("orangepeelui/uiskin.json"));
 		Gdx.input.setInputProcessor(stage);
 
-		table = new Table();
-		table.setFillParent(true);
-		stage.addActor(table);
+		root = new Table();
+		root.setFillParent(true);
+		stage.addActor(root);
 
+		Table table = new Table();
+		root.add(table).growX().align(Align.top).colspan(4);
 		menuButton = new Button(skin);
-		table.add(menuButton).align(Align.top | Align.left).pad(10).expand();
+		table.add(menuButton).align(Align.top | Align.left).padTop(10).expandX();
 
 		Button healthBar1 = new Button(skin);
-		table.add(healthBar1).align(Align.top|Align.right).pad(10).expand().width(Value.percentWidth(8f, menuButton));
+		table.add(healthBar1).align(Align.top).padTop(10).width(Value.percentWidth(7f, menuButton));
 		
 		Button vs = new Button(skin);
-		table.add(vs).align(Align.top).pad(10).expandY().width(Value.percentWidth(1f, menuButton));
+		table.add(vs).align(Align.top).pad(10).width(Value.percentWidth(1f, menuButton));
 
 		Button healthBar2 = new Button(skin);
-		table.add(healthBar2).align(Align.top | Align.left).pad(10).expand().width(Value.percentWidth(8f, menuButton));
-		table.add().expand().pad(35);
+		table.add(healthBar2).align(Align.top).padTop(10).width(Value.percentWidth(7f, menuButton));
 
+		Button invisButton = new Button(skin);
+		invisButton.setColor(0.5f, 0.5f, 0.5f, 0f);
+		table.add(invisButton).align(Align.top | Align.right).padTop(10).width(Value.percentWidth(1f, menuButton)).expandX();
+
+		root.row();
+
+		Button fuelBar = new Button(skin);
+		root.add(fuelBar).align(Align.left).padLeft(30).padTop(80).expandX().width(Value.percentWidth(4f, menuButton)).height(Value.percentHeight(0.5f,menuButton)).expandY();
+		
+		Button selector = new Button(skin);
+		root.add(selector).width(Value.percentWidth(2f, menuButton)).height(Value.percentHeight(1.5f,menuButton)).padTop(80).expandY();
+
+		Button fireButton = new Button(skin);
+		root.add(fireButton).align(Align.left).padLeft(5).padTop(80).width(Value.percentWidth(2f, menuButton)).height(Value.percentHeight(1.5f,menuButton)).expandY();
+
+		root.add().expandX().width(Value.percentWidth(1f,fuelBar)).expandY();
+
+
+
+
+		root.row();
 	}
 
 	@Override
