8dfde3a651738af05574dd9263d47608dcee632d
divyajeettt <divyajeet21529@iiitd.ac.in>
2022-12-10 17:51:01 +0530

    Added ability to choose tanks for both players

--- core/src/com/dragonjeet/tankstars/menu/SelectionMenu.java
+++ core/src/com/dragonjeet/tankstars/menu/SelectionMenu.java
@@ -22,85 +22,21 @@ public class SelectionMenu extends Menu {
     private int player = 1;        // player=1 => Player-1's turn, player=0 => Player-2's turn; player ^= 1 after every iteration
     private TextureRegion selectedTankImage;
     private TextureRegion selectedTankName;
+    private Table table;
+    private Table childTable;
+    private Button tank1, tank2, tank3;
+    private ImageButton actionButton;   // for player 1, this will be "Next" button, for player 2, this will be "Play" button
 
     public SelectionMenu(final TankStars game) {
         super(game);
+        setTable();
 
         background = new Texture(Gdx.files.internal("backgrounds/selectionMenu.png"));
-        selectedTankImage = new TextureRegion(new Texture(Gdx.files.internal("tanks/tank-1/full.png")));
-        selectedTankName = new TextureRegion(new Texture(Gdx.files.internal("tanks/tank-1/name.png")));
+        selectTank(1);  // default selected tank
 
-        game.setTank(new Tank1(0, game.getGround(), (player != 1)), player);
-
-        Table table = new Table();
-        table.setFillParent(true);
-        stage.addActor(table);
-
-        table.add().grow();
-        table.add().grow();
-        Table childTable = new Table();
-        table.add(childTable).expandY();
-
-        // Do something to fix the positioning of the tank buttons
-        // I have added them to a table like before
-
-        ImageButton playButton = new ImageButton(
-            new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("buttons/up/play.png")))),
-            new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("buttons/down/play.png"))))
-        );
-
-        playButton.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                BattleScreen battleScreen = new BattleScreen(game);
-                game.setScreen(battleScreen);
-                dispose();
-            }
-        });
-
-        TextureRegionDrawable tankImage1 = new TextureRegionDrawable(new TextureRegion(new Texture("tanks/tank-1/full.png")));
-        Button tank1 = new Button(tankImage1, tankImage1);
-        tank1.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                selectedTankImage = new TextureRegion(new Texture("tanks/tank-1/full.png"));
-                selectedTankName = new TextureRegion(new Texture(Gdx.files.internal("tanks/tank-1/name.png")));
-                game.setTank(new Tank1(0, game.getGround(), (player != 1)), player);
-            }
-        });
-
-        TextureRegionDrawable tankImage2 = new TextureRegionDrawable(new TextureRegion(new Texture("tanks/tank-2/full.png")));
-        Button tank2 = new Button(tankImage2, tankImage2);
-        tank2.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                selectedTankImage = new TextureRegion(new Texture("tanks/tank-2/full.png"));
-                selectedTankName = new TextureRegion(new Texture(Gdx.files.internal("tanks/tank-2/name.png")));
-                game.setTank(new Tank2(0, game.getGround(), (player != 1)), player);
-            }
-        });
-
-        TextureRegionDrawable tankImage3 = new TextureRegionDrawable(new TextureRegion(new Texture("tanks/tank-3/full.png")));
-        Button tank3 = new Button(tankImage3, tankImage3);
-        tank3.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                selectedTankImage = new TextureRegion(new Texture("tanks/tank-3/full.png"));
-                selectedTankName = new TextureRegion(new Texture(Gdx.files.internal("tanks/tank-3/name.png")));
-                game.setTank(new Tank3(0, game.getGround(), (player != 1)), player);
-            }
-        });
-
-        table.columnDefaults(0).width(Gdx.graphics.getWidth() / 3f);
-
-        childTable.add(tank1).width(Value.percentWidth(1f, playButton)).height(Value.percentHeight(1f, playButton)).expandY().align(Align.bottom).padRight(20);
-        childTable.add(tank2).width(Value.percentWidth(1f, playButton)).height(Value.percentHeight(1f, playButton)).expandY().align(Align.bottom);
-        childTable.add(tank3).width(Value.percentWidth(1f, playButton)).height(Value.percentHeight(1f, playButton)).expandY().align(Align.bottom).padLeft(20);
-
-        childTable.row();
-        childTable.add(playButton).expand().align(Align.bottom).colspan(3).padTop(160);
-
-        setUi(table);
+        setActionButton();
+        setTankButtons();
+        placeObjects();
     }
 
     public void drawSelectedTank(SpriteBatch batch, TextureRegion tankImage, TextureRegion tankName) {
@@ -108,7 +44,6 @@ public class SelectionMenu extends Menu {
             tankImage, 150, 180, 0, 0,
             tankImage.getRegionWidth(), tankImage.getRegionHeight(), 2, 2, 0
         );
-
         batch.draw(
             tankName, 250, 700, 0, 0,
             tankName.getRegionWidth(), tankName.getRegionHeight(), 1.4f, 1.4f, 0
@@ -123,5 +58,100 @@ public class SelectionMenu extends Menu {
         game.batch.end();
         stage.act(d);
         stage.draw();
+    }
+
+    private void setTable() {
+        this.table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+        table.add().grow();
+        table.add().grow();
+        this.childTable = new Table();
+        table.add(childTable).expandY();
+    }
+
+    private void selectTank(int num) {
+        selectedTankImage = new TextureRegion(new Texture(Gdx.files.internal("tanks/tank-"+num+"/full.png")));
+        selectedTankName = new TextureRegion(new Texture(Gdx.files.internal("tanks/tank-"+num+"/name.png")));
+        if (num == 1) {
+            game.setTank(new Tank1(0, game.getGround(), (player != 1)), player);
+        } else if (num == 2) {
+            game.setTank(new Tank2(0, game.getGround(), (player != 1)), player);
+        } else {
+            game.setTank(new Tank3(0, game.getGround(), (player != 1)), player);
+        }
+    }
+
+    private TextureRegionDrawable getTankImage(int num) {
+        return new TextureRegionDrawable(new TextureRegion(new Texture("tanks/tank-"+num+"/full.png")));
+    }
+
+    private void setTankButtons() {
+        TextureRegionDrawable tankImage1 = getTankImage(1);
+        this.tank1 = new Button(tankImage1, tankImage1);
+        tank1.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                selectTank(1);
+            }
+        });
+
+        TextureRegionDrawable tankImage2 = getTankImage(2);
+        this.tank2 = new Button(tankImage2, tankImage2);
+        tank2.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                selectTank(2);
+            }
+        });
+
+        TextureRegionDrawable tankImage3 = getTankImage(3);
+        this.tank3 = new Button(tankImage3, tankImage3);
+        tank3.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                selectTank(3);
+            }
+        });
+    }
+
+    private void setActionButton() {
+        this.actionButton = new ImageButton(
+            new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("buttons/up/next.png")))),
+            new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("buttons/down/next.png"))))
+        );
+
+        actionButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                if (player == 1) {
+                    player ^= 1;
+                    actionButton.getStyle().imageUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("buttons/up/play.png"))));
+                    actionButton.getStyle().imageDown = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("buttons/down/play.png"))));
+                    setTankButtons();
+                } else {
+                    BattleScreen battleScreen = new BattleScreen(game);
+                    game.setScreen(battleScreen);
+                    dispose();
+                }
+            }
+        });
+    }
+
+    private void placeObjects() {
+        // Do something to fix the positioning of the tank buttons
+        // I have added them to a table like before
+
+        table.add(actionButton).expand().align(Align.bottom).padBottom(50);
+        table.columnDefaults(0).width(Gdx.graphics.getWidth() / 3f);
+
+        childTable.add(tank1).width(Value.percentWidth(1f, actionButton)).height(Value.percentHeight(1f, actionButton)).expandY().align(Align.bottom).padRight(20);
+        childTable.add(tank2).width(Value.percentWidth(1f, actionButton)).height(Value.percentHeight(1f, actionButton)).expandY().align(Align.bottom);
+        childTable.add(tank3).width(Value.percentWidth(1f, actionButton)).height(Value.percentHeight(1f, actionButton)).expandY().align(Align.bottom).padLeft(20);
+
+        childTable.row();
+        childTable.add(actionButton).expand().align(Align.bottom).colspan(3).padTop(160);
+
+        setUi(table);
     }
 }
